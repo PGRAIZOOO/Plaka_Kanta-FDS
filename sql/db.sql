dde35802a431fe453d220548ae8f03cd23196e77
cedrichanznunez <cedrichanzn@gmail.com>
2026-06-28 22:48:29 +0800

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -16,12 +16,6 @@ CREATE TABLE users (
     is_active TINYINT DEFAULT 1
 );
 
-drop table if EXISTS genres;
-
-CREATE TABLE genres (
-    genre_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    genre_name VARCHAR(50) NOT NULL
-);
 
 drop table if EXISTS products;
 
@@ -31,13 +25,11 @@ CREATE TABLE products (
     name VARCHAR(100) NOT NULL,
     artist_brand VARCHAR(100),
     product_type ENUM('CD', 'Vinyl', 'Record Player') NOT NULL,
-    genre_id INT NULL,
+    genre ENUM('Pop', 'Metal', 'Alternative', 'Country', 'Rock', 'Hip Hop'),
     unit_cost_price DECIMAL(10,2) NOT NULL,
     unit_selling_price DECIMAL(10,2) NOT NULL,
     min_stock_threshold INT DEFAULT 5,
-    is_active TINYINT DEFAULT 1,
-    INDEX(genre_id),
-    CONSTRAINT fk_product_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+    is_active TINYINT DEFAULT 1
 );
 
 drop table if EXISTS stock;
@@ -117,40 +109,141 @@ INSERT INTO users (username, password, role, full_name, contact_number, address,
 ('pmendoza', 'mendopass', 'Customer', 'Paula Mendoza', '09170010123', 'Rizal', 1),
 ('kramos', 'easypeazy', 'Customer', 'Kevin Ramos', '09170011234', 'Taguig', 1);
 
-INSERT INTO genres (genre_name) VALUES
-('Rock'), ('Pop'), ('Hip Hop'), ('Jazz'), ('Classical'),
-('R&B'), ('Electronic'), ('Indie'), ('Metal'), ('Reggae');
 
-INSERT INTO products (sku, name, artist_brand, product_type, genre_id, unit_cost_price, unit_selling_price, min_stock_threshold, is_active) VALUES
-('SKU1001','Abbey Road','The Beatles','Vinyl',1,500,950,5,1),
-('SKU1002','Thriller','Michael Jackson','CD',2,280,650,5,1),
-('SKU1003','DAMN.','Kendrick Lamar','CD',3,300,720,5,1),
-('SKU1004','Kind of Blue','Miles Davis','Vinyl',4,450,900,5,1),
-('SKU1005','Beethoven 9th','Ludwig van Beethoven','CD',5,220,500,5,1),
+INSERT INTO products (sku, name, artist_brand, product_type, genre, unit_cost_price, unit_selling_price, min_stock_threshold, is_active) VALUES
 
-('SKU1006','After Hours','The Weeknd','CD',6,320,750,5,1),
-('SKU1007','Random Access Memories','Daft Punk','Vinyl',7,480,980,5,1),
-('SKU1008','Indie Essentials','Various Artists','CD',8,180,420,5,1),
-('SKU1009','Master of Puppets','Metallica','Vinyl',9,520,1100,5,1),
-('SKU1010','Exodus','Bob Marley','CD',10,260,600,5,1),
+-- pop vinyls
+('VNY1001', 'Harry''s House', 'Harry Styles', 'Vinyl', 'Pop', 1260, 3150, 5, 1),
+('VNY1002', '1989 (Taylor''s Version)', 'Taylor Swift', 'Vinyl', 'Pop', 1800, 4500, 5, 1),
+('VNY1003', 'a beautiful blur', 'LANY', 'Vinyl', 'Pop', 900, 2250, 5, 1),
+('VNY1004', 'SOUR', 'Olivia Rodrigo', 'Vinyl', 'Pop', 1200, 3000, 5, 1),
+('VNY1005', 'brat', 'Charli XCX', 'Vinyl', 'Pop', 1200, 3000, 5, 1),
+('VNY1006', 'Short n'' Sweet', 'Sabrina Carpenter', 'Vinyl', 'Pop', 1300, 3250, 5, 1),
+('VNY1007', 'thank u, next', 'Ariana Grande', 'Vinyl', 'Pop', 1200, 3000, 5, 1),
+('VNY1008', 'Changes', 'Justin Bieber', 'Vinyl', 'Pop', 1000, 2500, 5, 1),
+('VNY1009', 'Bad', 'Michael Jackson', 'Vinyl', 'Pop', 800, 2000, 5, 1),
+('VNY1010', 'Thriller', 'Michael Jackson', 'Vinyl', 'Pop', 800, 2000, 5, 1),
+('VNY1011', '...baby one more time', 'Britney Spears', 'Vinyl', 'Pop', 800, 2000, 5, 1),
+('VNY1012', 'Like a Prayer', 'Madonna', 'Vinyl', 'Pop', 800, 2000, 5, 1),
+('VNY1013', 'Teenage Dream', 'Katy Perry', 'Vinyl', 'Pop', 1400, 3500, 5, 1),
+('VNY1014', '24K Magic', 'Bruno Mars', 'Vinyl', 'Pop', 800, 2000, 5, 1),
+('VNY1015', 'Melodrama', 'Lorde', 'Vinyl', 'Pop', 800, 2000, 5, 1),
 
-('SKU1011','The Wall','Pink Floyd','Vinyl',1,550,1150,5,1),
-('SKU1012','1989','Taylor Swift','CD',2,290,690,5,1),
-('SKU1013','Astroworld','Travis Scott','CD',3,310,730,5,1),
-('SKU1014','Blue Train','John Coltrane','Vinyl',4,460,920,5,1),
-('SKU1015','Mozart Collection','Mozart','CD',5,240,540,5,1),
+-- metal vinyls
+('VNY2001', 'Ride the Lightning', 'Metallica', 'Vinyl', 'Metal', 800, 2000, 5, 1),
+('VNY2002', 'Master of Puppets', 'Metallica', 'Vinyl', 'Metal', 800, 2000, 5, 1),
+('VNY2003', 'Painkiller', 'Judas Priest', 'Vinyl', 'Metal', 800, 2000, 5, 1),
+('VNY2004', 'Paranoid', 'Black Sabbath', 'Vinyl', 'Metal', 600, 1500, 5, 1),
+('VNY2005', 'Fallen', 'Evanescence', 'Vinyl', 'Metal', 600, 1500, 5, 1),
+('VNY2006', 'Fortitude', 'Gojira', 'Vinyl', 'Metal', 600, 1500, 5, 1),
 
-('SKU1016','Starboy','The Weeknd','CD',6,300,700,5,1),
-('SKU1017','Discovery','Daft Punk','Vinyl',7,470,970,5,1),
-('SKU1018','Indie Waves','Various Artists','CD',8,200,450,5,1),
-('SKU1019','Ride the Lightning','Metallica','Vinyl',9,530,1120,5,1),
-('SKU1020','Legend','Bob Marley','CD',10,270,620,5,1);
+-- alternative vinyls
+('VNY3001', 'Eyes Open', 'Snow Patrol', 'Vinyl', 'Alternative', 1260, 3150, 5, 1),
+('VNY3002', 'OK Computer', 'Radiohead', 'Vinyl', 'Alternative', 1400, 3500, 5, 1),
+('VNY3003', 'Hatful of Hollow', 'The Smiths', 'Vinyl', 'Alternative', 800, 2000, 5, 1),
+('VNY3004', 'Ghost Stories', 'Coldplay', 'Vinyl', 'Alternative', 800, 2000, 5, 1),
+('VNY3005', 'Folklore', 'Taylor Swift', 'Vinyl', 'Alternative', 1500, 3750, 5, 1),
+('VNY3006', 'Pablo Honey', 'Radiohead', 'Vinyl', 'Alternative', 800, 2000, 5, 1),
+
+-- country vinyls
+('VNY4001', 'Fearless (Platinum Edition)', 'Taylor Swift', 'Vinyl', 'Country', 1400, 3500, 5, 1),
+('VNY4002', 'Come On Over', 'Shania Twain', 'Vinyl', 'Country', 1180, 2950, 5, 1),
+('VNY4003', 'At Folsom Prison', 'Johnny Cash', 'Vinyl', 'Country', 600, 1500, 5, 1),
+('VNY4004', 'Traveller', 'Chris Stapleton', 'Vinyl', 'Country', 800, 2000, 5, 1),
+('VNY4005', 'Standing Room Only', 'Tim McGraw', 'Vinyl', 'Country', 800, 2000, 5, 1),
+('VNY4006', 'I''m the Problem', 'Morgan Wallen', 'Vinyl', 'Country', 1200, 3000, 5, 1),
+('VNY4007', 'Jolene', 'Dolly Parton', 'Vinyl', 'Country', 800, 2000, 5, 1),
+('VNY4008', 'The Very Best of Dolly Parton', 'Dolly Parton', 'Vinyl', 'Country', 1200, 3000, 5, 1),
+('VNY4009', 'Kansas Anymore', 'Role Model', 'Vinyl', 'Country', 900, 2250, 5, 1),
+
+-- rock vinyls
+('VNY5001', 'Use Your Illusion I', 'Guns N'' Roses', 'Vinyl', 'Rock', 1300, 3250, 5, 1),
+('VNY5002', 'Appetite for Destruction', 'Guns N'' Roses', 'Vinyl', 'Rock', 800, 2000, 5, 1),
+('VNY5003', 'Slippery When Wet', 'Bon Jovi', 'Vinyl', 'Rock', 800, 2000, 5, 1),
+('VNY5004', 'Abbey Road', 'The Beatles', 'Vinyl', 'Rock', 1000, 2500, 5, 1),
+('VNY5005', 'Rumours', 'Fleetwood Mac', 'Vinyl', 'Rock', 1100, 2750, 5, 1),
+('VNY5006', 'The Game', 'Queen', 'Vinyl', 'Rock', 900, 2250, 5, 1),
+('VNY5007', 'Greatest Hits', 'Queen', 'Vinyl', 'Rock', 1400, 3500, 5, 1),
+('VNY5008', 'Nevermind', 'Nirvana', 'Vinyl', 'Rock', 900, 2250, 5, 1),
+('VNY5009', 'Three Cheers for Sweet Revenge', 'My Chemical Romance', 'Vinyl', 'Rock', 900, 2250, 5, 1),
+
+-- hip hop vinyls
+('VNY6001', 'GNX', 'Kendrick Lamar', 'Vinyl', 'Hip Hop', 1300, 3250, 5, 1),
+('VNY6002', 'Astroworld', 'Travis Scott', 'Vinyl', 'Hip Hop', 1200, 3000, 5, 1),
+('VNY6003', 'ANTI', 'Rihanna', 'Vinyl', 'Hip Hop', 1400, 3500, 5, 1),
+('VNY6004', 'The Marshall Mathers LP', 'Eminem', 'Vinyl', 'Hip Hop', 1180, 2950, 5, 1),
+('VNY6005', 'The Blueprint', 'Jay-Z', 'Vinyl', 'Hip Hop', 1180, 2950, 5, 1),
+('VNY6006', 'The College Dropout', 'Kanye West', 'Vinyl', 'Hip Hop', 1200, 3000, 5, 1),
+
+-- pop cds
+('CD1001', 'Harry''s House', 'Harry Styles', 'CD', 'Pop', 500, 1250, 5, 1),
+('CD1002', 'reputation', 'Taylor Swift', 'CD', 'Pop', 400, 1000, 5, 1),
+('CD1003', '1989', 'Taylor Swift', 'CD', 'Pop', 380, 950, 5, 1),
+('CD1004', 'SOUR', 'Olivia Rodrigo', 'CD', 'Pop', 380, 950, 5, 1),
+('CD1005', 'brat', 'Charli XCX', 'CD', 'Pop', 500, 1250, 5, 1),
+('CD1006', 'Short n'' Sweet', 'Sabrina Carpenter', 'CD', 'Pop', 500, 1250, 5, 1),
+('CD1007', 'thank u, next', 'Ariana Grande', 'CD', 'Pop', 380, 950, 5, 1),
+('CD1008', 'positions', 'Ariana Grande', 'CD', 'Pop', 380, 950, 5, 1),
+('CD1009', 'Bad', 'Michael Jackson', 'CD', 'Pop', 340, 850, 5, 1),
+('CD1010', 'Thriller', 'Michael Jackson', 'CD', 'Pop', 340, 850, 5, 1),
+('CD1011', '24K Magic', 'Bruno Mars', 'CD', 'Pop', 340, 850, 5, 1),
+('CD1012', 'My World', 'Justin Bieber', 'CD', 'Pop', 300, 750, 5, 1),
+
+-- metal cds
+('CD2001', 'Ride the Lightning', 'Metallica', 'CD', 'Metal', 380, 950, 5, 1),
+('CD2002', 'Master of Puppets', 'Metallica', 'CD', 'Metal', 380, 950, 5, 1),
+('CD2003', 'Fallen', 'Evanescence', 'CD', 'Metal', 340, 850, 5, 1),
+
+-- alternative cds
+('CD3001', 'OK Computer', 'Radiohead', 'CD', 'Alternative', 460, 1150, 5, 1),
+('CD3002', 'Ghost Stories', 'Coldplay', 'CD', 'Alternative', 380, 950, 5, 1),
+('CD3003', 'Folklore', 'Taylor Swift', 'CD', 'Alternative', 500, 1250, 5, 1),
+
+-- country cds
+('CD4001', 'Speak Now', 'Taylor Swift', 'CD', 'Country', 380, 950, 5, 1),
+('CD4002', 'Fearless', 'Taylor Swift', 'CD', 'Country', 380, 950, 5, 1),
+('CD4003', 'I''m the Problem', 'Morgan Wallen', 'CD', 'Country', 500, 1250, 5, 1),
+('CD4004', 'Jolene', 'Dolly Parton', 'CD', 'Country', 340, 850, 5, 1),
+('CD4005', 'Kansas Anymore', 'Role Model', 'CD', 'Country', 460, 1150, 5, 1),
+('CD4006', 'Cowboy Carter', 'Beyoncé', 'CD', 'Country', 540, 1350, 5, 1),
+
+-- rock cds
+('CD5001', 'Use Your Illusion I', 'Guns N'' Roses', 'CD', 'Rock', 380, 950, 5, 1),
+('CD5002', 'Slippery When Wet', 'Bon Jovi', 'CD', 'Rock', 340, 850, 5, 1),
+('CD5003', 'Rumours', 'Fleetwood Mac', 'CD', 'Rock', 380, 950, 5, 1),
+('CD5004', 'Abbey Road', 'The Beatles', 'CD', 'Rock', 460, 1150, 5, 1),
+('CD5005', 'Nevermind', 'Nirvana', 'CD', 'Rock', 380, 950, 5, 1),
+('CD5006', 'Three Cheers for Sweet Revenge', 'My Chemical Romance', 'CD', 'Rock', 380, 950, 5, 1),
+
+-- hip hop cds
+('CD6001', 'GNX', 'Kendrick Lamar', 'CD', 'Hip Hop', 540, 1350, 5, 1),
+('CD6002', 'Astroworld', 'Travis Scott', 'CD', 'Hip Hop', 380, 950, 5, 1),
+('CD6003', 'ANTI', 'Rihanna', 'CD', 'Hip Hop', 460, 1150, 5, 1),
+('CD6004', 'The Eminem Show', 'Eminem', 'CD', 'Hip Hop', 380, 950, 5, 1),
+('CD6005', 'The College Dropout', 'Kanye West', 'CD', 'Hip Hop', 460, 1150, 5, 1),
+('CD6006', 'Graduation', 'Kanye West', 'CD', 'Hip Hop', 460, 1150, 5, 1),
+
+-- record players ( NULL yung genre since hindi naman yan genre )
+('RP0001', 'Stylish Wood Turntable', 'Audio-Technica', 'Record Player', NULL, 10600, 26499, 5, 1),
+('RP0002', 'Turntable with High-Fidelity Heritage', 'Audio-Technica', 'Record Player', NULL, 12800, 32000, 5, 1),
+('RP0003', 'Straight Carbon-fiber Tonearm with Bluetooth', 'Audio-Technica', 'Record Player', NULL, 16000, 39999, 5, 1),
+('RP0004', 'AT-VM95C Dual Magnet with Bluetooth (Black)', 'Audio-Technica', 'Record Player', NULL, 7600, 18999, 5, 1),
+('RP0005', 'AT-VM95C Dual Magnet with Bluetooth (White)', 'Audio-Technica', 'Record Player', NULL, 7600, 18999, 5, 1),
+('RP0006', 'Audio-Technica USB and Analog Output', 'Audio-Technica', 'Record Player', NULL, 5200, 12999, 5, 1);
+
+-- stock for all 99 fucking products 
 
 INSERT INTO stock (product_id, quantity) VALUES
-(1, 38),(2, 52),(3, 27),(4, 44),(5, 61),
-(6, 33),(7, 21),(8, 57),(9, 18),(10, 70),
-(11, 40),(12, 49),(13, 26),(14, 35),(15, 63),
-(16, 31),(17, 24),(18, 58),(19, 19),(20, 66);
+(1,55),(2,22),(3,16),(4,62),(5,32),(6,30),(7,29),(8,23),(9,62),(10,21),
+(11,58),(12,62),(13,72),(14,49),(15,20),(16,52),(17,42),(18,17),(19,16),(20,20),
+(21,28),(22,29),(23,47),(24,53),(25,16),(26,50),(27,27),(28,60),(29,56),(30,59),
+(31,49),(32,41),(33,29),(34,43),(35,52),(36,32),(37,66),(38,70),(39,15),(40,63),
+(41,66),(42,25),(43,59),(44,42),(45,36),(46,32),(47,24),(48,28),(49,63),(50,36),
+(51,21),(52,20),(53,39),(54,21),(55,37),(56,69),(57,37),(58,53),(59,31),(60,66),
+(61,17),(62,61),(63,44),(64,49),(65,22),(66,74),(67,39),(68,20),(69,50),(70,33),
+(71,68),(72,55),(73,54),(74,71),(75,70),(76,38),(77,51),(78,27),(79,60),(80,19),
+(81,17),(82,57),(83,29),(84,64),(85,33),(86,20),(87,69),(88,29),(89,70),(90,21),
+(91,39),(92,32),(93,44),(94,55),(95,68),(96,38),(97,25),(98,38),(99,37);
 
 INSERT INTO stock_movements (product_id, quantity_change, reason, movement_date) VALUES
 (1, 20, 'New shipment received', '2026-06-01 10:00:00'),
